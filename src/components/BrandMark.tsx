5b6e5627cb309a0c40e11fd67cc1c7121aaa5ce6
LexusG <alexgrilexus@gmail.com>
2026-07-04 14:51:37 -0500

    Use circular treatment for GENR8 mark

--- src/components/BrandMark.tsx
+++ src/components/BrandMark.tsx
@@ -8,7 +8,7 @@ export function BrandMark({ className = '', alt = 'GENR8', ...props }: BrandMark
     <img
       src="/brand/g3d-mark-512.png"
       alt={alt}
-      className={`shrink-0 rounded-[26%] bg-[#1b1a1a] object-cover shadow-sm ring-1 ring-black/10 dark:ring-white/15 ${className}`}
+      className={`shrink-0 rounded-full bg-[#1b1a1a] object-cover shadow-sm ring-1 ring-black/10 dark:ring-white/15 ${className}`}
       draggable={false}
       {...props}
     />
